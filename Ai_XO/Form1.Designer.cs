0f73584b89cbdcc0129473bab5b01118110c3313
ShadiAlhalak <shadialhalak0@gmail.com>
2022-01-10 17:22:04 +0200

    Add project files.

---
+++ Ai_XO/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Ai_XO
+{
+    partial class frmXO
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTicTacToy = new System.Windows.Forms.Label();
+            this.grpGrid = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.grpGrid.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.lblTicTacToy, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.grpGrid, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(353, 514);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblTicTacToy
+            // 
+            this.lblTicTacToy.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTicTacToy.AutoSize = true;
+            this.lblTicTacToy.BackColor = System.Drawing.Color.LightGray;
+            this.lblTicTacToy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTicTacToy.Font = new System.Drawing.Font("Stencil", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTicTacToy.Location = new System.Drawing.Point(8, 5);
+            this.lblTicTacToy.Name = "lblTicTacToy";
+            this.lblTicTacToy.Size = new System.Drawing.Size(337, 100);
+            this.lblTicTacToy.TabIndex = 0;
+            this.lblTicTacToy.Text = "Tic Tac ToE";
+            this.lblTicTacToy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grpGrid
+            // 
+            this.grpGrid.BackColor = System.Drawing.Color.Transparent;
+            this.grpGrid.Controls.Add(this.tableLayoutPanel2);
+            this.grpGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpGrid.Location = new System.Drawing.Point(8, 128);
+            this.grpGrid.Name = "grpGrid";
+            this.grpGrid.Size = new System.Drawing.Size(337, 358);
+            this.grpGrid.TabIndex = 1;
+            this.grpGrid.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel2.Controls.Add(this.btn1, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btn2, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btn3, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btn4, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btn5, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btn6, 3, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btn7, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.btn8, 2, 3);
+            this.tableLayoutPanel2.Controls.Add(this.btn9, 3, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 23);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(331, 332);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn1.FlatAppearance.BorderSize = 5;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(8, 8);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(101, 101);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn2.FlatAppearance.BorderSize = 5;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn2.Location = new System.Drawing.Point(115, 8);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(101, 101);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn3.FlatAppearance.BorderSize = 5;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(222, 8);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(101, 101);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn4.FlatAppearance.BorderSize = 5;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(8, 115);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(101, 101);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn5.FlatAppearance.BorderSize = 5;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(115, 115);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(101, 101);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn6.FlatAppearance.BorderSize = 5;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(222, 115);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(101, 101);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn7.FlatAppearance.BorderSize = 5;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(8, 222);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(101, 101);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn8.FlatAppearance.BorderSize = 5;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(115, 222);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(101, 101);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn9.FlatAppearance.BorderSize = 5;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Stencil", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(222, 222);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(101, 101);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            // 
+            // frmXO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 514);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(371, 561);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(371, 561);
+            this.Name = "frmXO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X/O";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.grpGrid.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblTicTacToy;
+        private System.Windows.Forms.GroupBox grpGrid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+    }
+}
---
+++ Ai_XO/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ai_XO
+{
+    public partial class frmXO : Form
+    {
+        #region Variable And Constant
+        String[,] board = new string[3, 3];
+        #endregion
+
+        #region Constructor
+
+        public frmXO()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = "";
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void btn1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button temp = (Button)sender;
+            if (string.IsNullOrEmpty(temp.Text))
+            {
+
+                temp.Text = "X";
+                SetX(temp.Name);
+                Move();
+                System.Diagnostics.Debug.Write("\n");
+            }
+        }
+
+        #endregion
+
+        #region Methods And Functions 
+  
+
+        String CheckWinner()
+        {
+            if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2] && board[0, 0] != "")
+                return board[0, 0];
+            else if (board[1, 0] == board[1, 1] && board[1, 0] == board[1, 2] && board[1, 0] != "")
+                return board[1, 0];
+            else if (board[2, 0] == board[2, 1] && board[2, 0] == board[2, 2] && board[2, 0] != "")
+                return board[2, 0];
+            else if (board[0, 0] == board[1, 0] && board[0, 0] == board[2, 0] && board[0, 0] != "")
+                return board[0, 0];
+            else if (board[0, 1] == board[1, 1] && board[0, 1] == board[2, 1] && board[0, 1] != "")
+                return board[0, 1];
+            else if (board[0, 2] == board[1, 2] && board[0, 2] == board[2, 2] && board[0, 2] != "")
+                return board[0, 2];
+            else if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2] && board[0, 0] != "")
+                return board[0, 0];
+            else if (board[2, 0] == board[1, 1] && board[2, 0] == board[0, 2] && board[2, 0] != "")
+                return board[2, 0];
+            else
+                return "";
+        }
+
+        Boolean  checkDraw()
+        {
+            for (int i = 0 ; i < 3 ; i++)
+            {
+                for (int j=0; j < 3; j++)
+                {
+                    if (board[i, j] == "")
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+         }
+
+        Boolean CheckAvalibal(int i, int j)
+        {
+            if (board[i, j] == "")
+            { 
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        void SetX(String btn)
+        {
+            if (btn == "btn1")
+            {
+                board[0, 0] = "X";
+            }
+            else if (btn == "btn2")
+            {
+                board[0, 1] = "X";
+            }
+            else if (btn == "btn3")
+            {
+                board[0, 2] = "X";
+            }
+            else if (btn == "btn4")
+            {
+                board[1, 0] = "X";
+            }
+            else if (btn == "btn5")
+            {
+                board[1, 1] = "X";
+            }
+            else if (btn == "btn6")
+            {
+                board[1, 2] = "X";
+            }
+            else if (btn == "btn7")
+            {
+                board[2, 0] = "X";
+            }
+            else if (btn == "btn8")
+            {
+                board[2, 1] = "X";
+            }
+            else if (btn == "btn9")
+            {
+                board[2, 2] = "X";
+            }
+
+        }
+
+        void SetO(int i, int j)
+        {
+            if (CheckAvalibal(i, j))
+            {
+                if (i == 0 && j == 0)
+                {
+                    btn1.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 0 && j == 1)
+                {
+                    btn2.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 0 && j == 2)
+                {
+                    btn3.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 1 && j == 0)
+                {
+                    btn4.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 1 && j == 1)
+                {
+                    btn5.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 1 && j == 2)
+                {
+                    btn6.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 2 && j == 0)
+                {
+                    btn7.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 2 && j == 1)
+                {
+                    btn8.Text = "O";
+                    board[i, j] = "O";
+                }
+                else if (i == 2 && j == 2)
+                {
+                    btn9.Text = "O";
+                    board[i, j] = "O";
+                }
+            }
+        }
+
+        void clear()
+        {
+            board = new string[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i,j] = "";
+                }
+            }
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+        }
+
+        void Move()
+        {
+            int m = 0;
+            int n = 0;
+            int bestScore = int.MinValue;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "")
+                    {
+                        board[i, j] = "O";
+                        int score = minimax(board, 0, false);
+                        System.Diagnostics.Debug.Write("a[" + i + "," + j + "]=" + board[i, j] + "Score = " + score + " , ");
+                        board[i, j] = "";
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            m = i;
+                            n = j;
+                        }
+                        //else {
+                        //    System.Diagnostics.Debug.WriteLine("score="+score +" , BestScore="+bestScore );
+                        //}
+                    }
+                }
+                System.Diagnostics.Debug.Write("\n");
+            }
+
+            SetO(m, n);
+            String Winner = CheckWinner();
+            if (Winner != "")
+            {
+                MessageBox.Show("Winer : " + Winner);
+                clear();
+            }
+            else if (checkDraw())
+            {
+                MessageBox.Show("   Draw    ");
+                clear();
+            }
+        }
+
+        int minimax(String[,] board, int depth, Boolean isMaximizing)
+        {
+            int bestScore;
+            if (CheckWinner() == "O")
+            {
+                return 1;
+            }
+            else if (CheckWinner() == "X")
+            {
+                return -1;
+            }
+            else if (checkDraw())
+                return 0;
+
+            if (isMaximizing)
+            {
+                bestScore = int.MinValue;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == "")
+                        {
+                            board[i, j] = "O";
+                            int score = minimax(board, depth + 1, false);
+                            board[i, j] = "";
+                            if (score > bestScore)
+                                bestScore = score;
+                        }
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                bestScore = int.MaxValue;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == "")
+                        {
+                            board[i, j] = "X";
+                            int score = minimax(board, depth + 1, true);
+                            board[i, j] = "";
+                            if (score < bestScore)
+                                bestScore = score;
+                        }
+                    }
+                }
+                return bestScore;
+            }
+
+        }
+
+        #endregion
+
+    }
+}
